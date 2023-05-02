159cae46be3a3052b849b992543a36fd582c848a
Marcos Vinicius de Quadros <98984018+MarcosQuadross@users.noreply.github.com>
2023-05-02 20:36:35 -0300

    delete teste directory

--- testes/entrada.dart
+++
@@ -1,7 +0,0 @@
-import 'model.dart';
-
-class Informacoes {
-  Frase nome = Frase(tamanho: 50, verboso: 'nome completo');
-  Texto bio = Texto(verboso: 'biografia');
-  Data ultima_atualizacao = Data();
-}
